ca55ddae67dcc3508459dc29ff2aabea2786e5c8
adadofr <thioff1@gmail.com>
2023-07-04 17:18:57 +0200

    first commit

---
+++ Hangar.java
@@ -0,0 +1,8 @@
+public class Hangar {
+  public static void main(String[] args) {
+    Car clio = new Car("Clio", 80000);
+    Boat titanic = new Boat("Titanic", 0);
+    System.out.println(clio.doStuff());
+    System.out.println(titanic.doStuff());
+  }
+}
